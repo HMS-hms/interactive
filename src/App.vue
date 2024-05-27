94e8906bf3d18d1a21daba01516ff31790254f64
hcj <980135019@qq.com>
2024-05-27 16:51:15 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ChildView></ChildView>
+</template>
+
+<script>
+import ChildView from './components/ChildView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ChildView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
